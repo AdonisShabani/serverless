0260279ffb358af6fffcf566b47064b5bbc691f6
AdonisShabani <adonisshabani12@gmail.com>
2023-08-24 13:51:56 +0200

    update policy for lambda

--- terraform/serverless/data.tf
+++ terraform/serverless/data.tf
@@ -45,7 +45,8 @@ data "aws_iam_policy_document" "lambda_access_policy" {
     effect = "Allow"
     actions = [
       "ec2:CreateNetworkInterface",
-      "ec2:DescribeNetworkInterfaces"
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface"
     ]
     resources = [
       "*"
