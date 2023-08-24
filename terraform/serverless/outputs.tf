9a53f546164b7be5c86b6c67d7817afb7a5f2fc8
AdonisShabani <adonisshabani12@gmail.com>
2023-08-24 14:20:29 +0200

    add output variable

--- terraform/serverless/outputs.tf
+++ terraform/serverless/outputs.tf
@@ -1,16 +1,18 @@
-# output "lambda_function_arn" {
-#   description = "The ARN of the Lambda Function"
-#   value       = aws_lambda_function.lambda[each.key].arn
-# }
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda Function"
+  value = [
+    for k in aws_lambda_function.lambda : k.name
+  ]
+}
 
 # output "lambda_function_invoke_arn" {
 #   description = "The Invoke ARN of the Lambda Function"
-#   value       = aws_lambda_function.lambda[each.key].invoke_arn
+#   value       = aws_lambda_function.lambda[*].invoke_arn
 # }
 
 # output "lambda_function_name" {
 #   description = "The name of the Lambda Function"
-#   value       = aws_lambda_function.lambda[each.key].function_name
+#   value       = aws_lambda_function.lambda[*].function_name
 # }
 
 # output "lambda_function_version" {
