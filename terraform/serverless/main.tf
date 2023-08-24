8b00f4c7eae66a057070d3297190be4975b3eca7
AdonisShabani <adonisshabani12@gmail.com>
2023-08-24 11:39:49 +0200

    minor fixes

--- terraform/serverless/main.tf
+++ terraform/serverless/main.tf
@@ -26,7 +26,7 @@ resource "aws_iam_policy" "policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.iam_for_lambda.arn
+  role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
