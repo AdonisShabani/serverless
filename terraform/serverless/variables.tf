2102ef6b50b169f252b307951ee2aa7af72abe58
AdonisShabani <adonisshabani12@gmail.com>
2023-09-01 11:53:06 +0200

    minor fixes

--- terraform/serverless/variables.tf
+++ terraform/serverless/variables.tf
@@ -91,7 +91,7 @@ variable "package_type" {
 variable "environment_variables" {
   description = "A map that defines environment variables for the Lambda Function."
   type        = map(string)
-  default     = null
+  default     = {}
 }
 
 variable "timeout" {
